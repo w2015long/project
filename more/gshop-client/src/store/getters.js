25cc196cfb1597ac08d7d1f8130e22506f48a006
Jsen <w2015long@qq.com>
2019-08-30 23:41:49 +0800

    ratings

--- more/gshop-client/src/store/getters.js
+++ more/gshop-client/src/store/getters.js
@@ -6,5 +6,9 @@ export default{
     },
     totalPrice (state) {
         return state.shopCart.reduce((pre,food) => pre + food.count * food.price , 0)
+    },
+    positiveSize (state) {
+        return state.ratings.reduce((pre,rating) => pre + (rating.rateType == 0 ? 1 : 0), 0)
     }
+
 }
